2ba40700b320fb81f6f228afee9bfe282ed495d3
Yoppey <apple@appurunoMacBook-Pro.local>
2023-02-27 10:10:56 +0900

    myFist commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
